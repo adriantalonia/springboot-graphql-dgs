2afe31dbe235ec13731abdc559d731effb0500b1
Adrian <adrian.talonia11@gmail.com>
2023-09-18 22:05:02 -0600

    add mapping

---
+++ graphql-demo/src/main/java/com/course/graphqldemo/util/HashUtil.java
@@ -0,0 +1,19 @@
+package com.course.graphqldemo.util;
+
+import org.bouncycastle.crypto.generators.OpenBSDBCrypt;
+
+import java.nio.charset.StandardCharsets;
+
+public class HashUtil {
+
+    private static final String BCRYPT_SALT = "dontDoThisOnProd";
+
+    public static boolean isBcryptMatch(String original, String hashValue) {
+        return OpenBSDBCrypt.checkPassword(hashValue, original.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String hashBcrypt(String original) {
+        return OpenBSDBCrypt.generate(original.getBytes(StandardCharsets.UTF_8),
+                BCRYPT_SALT.getBytes(StandardCharsets.UTF_8), 5);
+    }
+}
