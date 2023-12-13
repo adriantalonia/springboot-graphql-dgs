50127793d8e5cb6118792622bbaf50c9742fe6ec
Adrian <adrian.talonia11@gmail.com>
2023-12-12 23:26:26 -0600

    add entity manufacturer

--- product/src/main/java/com/graph/product/entity/Manufacturer.java
+++ product/src/main/java/com/graph/product/entity/Manufacturer.java
@@ -1,4 +1,53 @@
 package com.graph.product.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "manufacturers")
 public class Manufacturer {
+
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String name;
+    private String originCountry;
+    private String description;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOriginCountry() {
+        return originCountry;
+    }
+
+    public void setOriginCountry(String originCountry) {
+        this.originCountry = originCountry;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
